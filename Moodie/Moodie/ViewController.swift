970353f17a6e4dbb2053c25397bad97c79460d91
Jiwoo <jiwoo12021@naver.com>
2025-07-29 09:21:22 +0900

    feat: 초기 셋팅

---
+++ Moodie/Moodie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moodie
+//
+//  Created by heojiwoo on 7/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
