b5a3c0ebc8cd526440e5a212df8aa3080da411df
Jiwoo <jiwoo12021@naver.com>
2025-07-30 16:16:12 +0900

    feat: 네비게이션 수정

---
+++ Moodie/HotChal/Scene/Chal/Coordinator/ChalCoordinator.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+final class ChalCoordinator: Coordinator {
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    var type: CoordinatorType { .favorite }
+
+    required init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let chalMainViewController = ChalMainViewController()
+        chalMainViewController.view.backgroundColor = .systemBackground
+        chalMainViewController.delegate = self
+        self.navigationController.viewControllers = [chalMainViewController]
+    
+    }
+
+    func next() {
+        let chalMain2 = ChalMain2()
+        chalMain2.delegate = self
+        navigationController.pushViewController(chalMain2, animated: true)
+        print("다음페이지")
+    }
+    
+    func next2() {
+        let chalMain3 = ChalMain3()
+        navigationController.pushViewController(chalMain3, animated: true)
+        print("다음페이지")
+    }
+    
+    
+}
+
+
