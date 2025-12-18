5f6b4d9bd12190c16797fd350ccaef848452cc3b
rrheon <yongheon0806@gmail.com>
2025-12-18 22:01:00 +0900

    refactor: 챌린지 playerview 수정

--- HotChal/HotChal/Coordinator/ChallengePlayerCoordinator.swift
+++ HotChal/HotChal/Coordinator/ChallengePlayerCoordinator.swift
@@ -53,6 +53,23 @@ class ChallengePlayerCoordinator: BaseCoordinator, GetKeyWindowProtocol, Challen
     }
   }
   
+  func handleShowModal(from vc: UIViewController & ModalViewControllerProtocol) {
+    let modalVC = ModalViewController()
+    modalVC.delegate = vc
+    modalVC.modalPresentationStyle = .pageSheet
+    
+    if let sheet = modalVC.sheetPresentationController {
+      if #available(iOS 16.0, *) {
+        sheet.detents = [.custom { _ in return 220 }]
+      } else {
+        sheet.detents = [.medium()]
+      }
+      sheet.prefersGrabberVisible = true
+      sheet.preferredCornerRadius = 20
+    }
+    vc.present(modalVC, animated: true)
+  }
+  
   // MARK: - Helpers
   
   
