74cf8aa296f486a834f48826c36d4f8180d0e5b2
rrheon <yongheon0806@gmail.com>
2025-12-18 22:01:47 +0900

    refactor: 챌린지 배우기화면 reactorkit 적용

--- HotChal/HotChal/Scene/Learn/Coordinator/HotChallLearnCoordinator.swift
+++ HotChal/HotChal/Scene/Learn/Coordinator/HotChallLearnCoordinator.swift
@@ -19,21 +19,5 @@ final class HotChallLearnCoordinator: ChallengePlayerCoordinator {
     
     self.navigationController.viewControllers = [HotChallLearnViewController]
   }
-  
-  func handleShowModal(from vc: UIViewController & ModalViewControllerProtocol) {
-    let modalVC = ModalViewController()
-    modalVC.delegate = vc
-    modalVC.modalPresentationStyle = .pageSheet
-    
-    if let sheet = modalVC.sheetPresentationController {
-      if #available(iOS 16.0, *) {
-        sheet.detents = [.custom { _ in return 220 }]
-      } else {
-        sheet.detents = [.medium()]
-      }
-      sheet.prefersGrabberVisible = true
-      sheet.preferredCornerRadius = 20
-    }
-    vc.present(modalVC, animated: true)
-  }
+
 }
